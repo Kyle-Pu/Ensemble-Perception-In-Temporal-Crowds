8c37a3fd97ead2d52f93b32588a2244425071e2e
alicezhang030 <53063545+alicezhang030@users.noreply.github.com>
2019-07-18 16:34:33 -0700

    Update ensemblePerceptionTemporalDisplay.m

--- ensemblePerceptionTemporalDisplay.m
+++ ensemblePerceptionTemporalDisplay.m
@@ -14,14 +14,15 @@ x_center = window_w/2;
 y_center = window_h/2;
 
 %% Determining Location on Screen to Display Images
-xVector = linspace(850, window_w - 850, 2);
-yVector = linspace(450, window_h - 450, 2)
+xVector = linspace(807, window_w-807, 2);
+%xVector = linspace(850, window_w - 850, 2);
+yVector = linspace(385, window_h-385, 2);
 [x, y] = meshgrid(xVector, yVector);
 
 cd('Face_Stimuli');
 
 %% Transparency Mask to Filter Excess Stimuli
-Mask_Plain = imread('mask.png');
+Mask_Plain = imread('mask.PNG');
 Mask_Plain = Mask_Plain(:, :, 1); % Only need one color channel of a grayscale image
 
 %% Parameter Settings
@@ -31,30 +32,29 @@ total_Images = 4; % Number of images in each scene
 tid = zeros(1, total_Images);  % Hold textures
 
 for i = 1 : total_Images
-    
-    tmp_bmp = imread("num2str(i)" + ".png");
-    tmp_bmp(:, :, 4) = Mask_Plain;
-    
-    Screen('DrawText', window, 'Loading...', x_center, y_center-25); 
-    Screen('DrawText', window, [int2str(int16(i*100/147)) '%'], x_center, y_center+25);
-     
-    % Make texture of the stimuli matrix
+    tmp_bmp = imread([num2str(i) '.PNG']);
+    tmp_bmp(:,:,4) = Mask_Plain;
     tid(i) = Screen('MakeTexture', window, tmp_bmp);
-    
+    Screen('DrawText', window, 'Loading...', x_center, y_center-25); % Write text to confirm loading of images
+    Screen('DrawText', window, [int2str(int16(i*100/147)) '%'], x_center, y_center+25); % Write text to confirm percentage complete
+    Screen('Flip', window); % Display text
 end
 
 image_size = size(tmp_bmp);
-w_img =  image_size(2); % image width
-h_img =  image_size(1); % image height
+w_img =  image_size(2)/2; % image width
+h_img =  image_size(1)/2; % image height
 
 xy_rect = [x(:)' - w_img / 2; y(:)' - h_img / 2; x(:)' + w_img / 2; y(:)' + h_img / 2];
 
 % Select random oranges from the image index vector "num_oranges" using
 % function "randsample"
-rand_images = randsample(total_Images, total_Images); 
+rand_images = randsample(1:total_Images, total_Images); 
 
 Screen('DrawTextures', window, tid(rand_images), [], xy_rect);
 Screen('Flip', window);
 WaitSecs(5);
+
+cd('../');
  
 Screen('CloseAll');
+
