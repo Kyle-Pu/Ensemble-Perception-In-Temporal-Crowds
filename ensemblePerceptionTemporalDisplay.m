ccab8ef6dc000301d217482edfeb6bafdd03b5f3
Kyle-Pu <kylez.pu@gmail.com>
2019-07-18 22:06:58 -0700

    Display pictures and separated variables for total number of images and number of images in each scene so the displayed pictures in each scene wouldn't be a small amount of morphs away from each other.

--- ensemblePerceptionTemporalDisplay.m
+++ ensemblePerceptionTemporalDisplay.m
@@ -21,18 +21,18 @@ yVector = linspace(385, window_h-385, 2);
 cd('Face_Stimuli');
 
 %% Transparency Mask to Filter Excess Stimuli
-Mask_Plain = imread('mask.PNG');
-Mask_Plain = Mask_Plain(:, :, 1); % Only need one color channel of a grayscale image
+Mask_Plain = imread('mask.png');
+Mask_Plain = Mask_Plain(:, :, 1); % Only need one color channel of a grayscale image since all channels are the same
 
 %% Parameter Settings
-total_Images = 4; % Number of images in each scene
+total_Images = 147; % Number of images in our dataset
 
 %% Loading Textures Into tid
-tid = zeros(1, total_Images);  % Hold textures
+tid = zeros(1, total_Images);  % Holds textures
 
 for i = 1 : total_Images
     tmp_bmp = imread([num2str(i) '.PNG']);
-    tmp_bmp(:,:,4) = Mask_Plain;
+    %tmp_bmp(:,:,4) = Mask_Plain;
     tid(i) = Screen('MakeTexture', window, tmp_bmp);
     Screen('DrawText', window, 'Loading...', x_center, y_center-25); % Write text to confirm loading of images
     Screen('DrawText', window, [int2str(int16(i*100/147)) '%'], x_center, y_center+25); % Write text to confirm percentage complete
@@ -40,14 +40,13 @@ for i = 1 : total_Images
 end
 
 image_size = size(tmp_bmp);
-w_img =  image_size(2)/2; % image width
-h_img =  image_size(1)/2; % image height
+w_img =  image_size(2) / 2; % image width
+h_img =  image_size(1) / 2; % image height
 
 xy_rect = [x(:)' - w_img / 2; y(:)' - h_img / 2; x(:)' + w_img / 2; y(:)' + h_img / 2];
 
-% Select random oranges from the image index vector "num_oranges" using
-% function "randsample"
-rand_images = randsample(1:total_Images, total_Images); 
+num_In_Scene = 4; % The number of images we display in each scene
+rand_images = randsample(total_Images, num_In_Scene); 
 
 Screen('DrawTextures', window, tid(rand_images), [], xy_rect);
 Screen('Flip', window);
