eed410ad6fbfd7536349f521e9d515da8748b2c7
Kyle-Pu <kylez.pu@gmail.com>
2019-07-24 15:16:22 -0700

    Directions

--- masterV2.m
+++ masterV2.m
@@ -38,10 +38,10 @@ for i = 1 : total_Images
     tid(i) = Screen('MakeTexture', window, tmp_bmp);
     Screen('DrawText', window, 'Loading...', x_center*0.0069, y_center*1.9178); % Write text to confirm loading of images
     Screen('DrawText', window, [int2str(int16(i*100/147)) '%', 0.0069, y_center*1.9111]); % Write text to confirm percentage complete
-    Screen('DrawText', window, 'Hello! Welcome to the Temporal Crowds Experiment.',x_center*0.6528, y_center) % User instruction page with proportionalized coordinates
-    Screen('DrawText',window,'In the following screen, four flashing faces will be displayed, one of which will have a different expression from the other three faces.',x_center*0.025, y_center*1.0556); 
-    Screen('DrawText',window,'Please identify which of the four faces is an outlier.',x_center*0.6646,y_center*1.111);
-    Screen('DrawText',window,'Then, please specifify how different the outlier looked by choosing "High" variance or "Low" variance in the final slide.',x_center*0.125, y_center*1.1667);
+    Screen('DrawText', window, 'Hello! Welcome to the Temporal Crowds Experiment.',x_center, y_center) % User instruction page with proportionalized coordinates
+    Screen('DrawText',window,'In the following screen, four flashing faces will be displayed, one of which change expression more than the other three faces.',x_center, y_center - 50); 
+    Screen('DrawText',window,'Please identify which of the four faces changes the most (is an outlier).',x_center,y_center - 100);
+    Screen('DrawText',window,'Then, please specifify how the outlier differed by choosing "High" variance/change or "Low" variance/change in the final slide.',x_center, y_center - 150);
     Screen('Flip', window); % Display text
 end
 
